50e002775d447b646ad08841a61f298f44a0998a
wangyong <wb_wangyong02@meituan.com>
2021-10-26 13:52:48 +0800

    添加使用栈实现队列

--- src/main/java/com/kevin/capture/arithmetic/samples/QueueByStack.java
+++ src/main/java/com/kevin/capture/arithmetic/samples/QueueByStack.java
@@ -10,13 +10,6 @@ import java.util.Stack;
  * 2.提供push、pop、peek、empty方法 2.1 push的时候将数据放入到数据栈中，
  * 2.2执行pop和peek的时候移动到临时栈中，执行完毕后，还需要将临时栈中的数据移动到数据栈中
  * <p>
- * 方案二:复杂度(O(1))
- * 1. 采用两个栈：一个是输出栈、一个是输入栈
- * 1.1 输出栈 pop、peek操作
- * 2.2 输入栈 push操作
- * 2.
- *
- * </p>
  *
  * @author kevin
  */
@@ -42,7 +35,7 @@ public class QueueByStack<T> {
             temp.push(data.pop());
         }
         //临时栈栈顶数据
-        T value = temp.firstElement();
+        T value = temp.peek();
         while (!temp.empty()) {
             data.push(temp.pop());
         }
@@ -75,14 +68,76 @@ public class QueueByStack<T> {
         queue.push("4");
         queue.push("5");
 
-        System.out.println(queue.pop());
-        System.out.println(queue.pop());
-        System.out.println(queue.pop());
-        System.out.println(queue.pop());
-        System.out.println(queue.pop());
-        System.out.println(queue.pop());
+//        System.out.println(queue.pop());
+//        System.out.println(queue.pop());
+//        System.out.println(queue.pop());
+//        System.out.println(queue.pop());
+//        System.out.println(queue.pop());
+//        System.out.println(queue.pop());
+
+        QueueByStack<String> queue2 = new QueueByStack<>();
+        queue2.push("1");
+        queue2.push("2");
+        queue2.push("3");
+        queue2.push("4");
+        queue2.push("5");
+
+        System.out.println(queue2.pop());
+        System.out.println(queue2.pop());
+        System.out.println(queue2.pop());
+        System.out.println(queue2.pop());
+        System.out.println(queue2.pop());
+        System.out.println(queue2.pop());
+
+    }
+
+}
+
+/**
+ * 解法二: 复杂度(O(1))
+ * * 1. 采用两个栈：一个是输出栈、一个是输入栈
+ * * 1.1 输出栈 pop、peek操作
+ * * 2.2 输入栈 push操作
+ *
+ * @param <T>
+ */
+class QueueByStack2<T> {
+
+    /**
+     * 输入栈
+     */
+    private final Stack<T> input = new Stack<>();
+
+    /**
+     * 输出栈
+     */
+    private final Stack<T> output = new Stack<>();
+
+    public void push(T value) {
+        input.push(value);
+    }
+
+    public T peek() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.peek();
+    }
+
+    public T pop() {
+        if (output.empty()) {
+            while (!input.empty()) {
+                output.push(input.pop());
+            }
+        }
+        return output.pop();
+    }
 
 
+    public boolean empty() {
+        return input.isEmpty() && output.empty();
     }
 
 }
